ba6d81047131f2b6ee3fe693280191baf2cb231f
IsabelaTeixeiraRodrigues <isabela.rodrigues@sptech.school>
2025-12-11 12:27:46 -0300

    models kpi e grafico

--- src/models/medidaModel.js
+++ src/models/medidaModel.js
@@ -1,50 +1,137 @@
 var database = require("../database/config");
 
-function buscarLuminosidade(id, limite_linhas) {
+function buscarLuminosidade(fk_quadros, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    FROM medida
-                    WHERE fk_aquario = ${id}
-                    ORDER BY id DESC LIMIT ${limite_linhas}`;
+	luminosidade,
+    fk_quadros
+    FROM medida
+    WHERE fk_quadros = ${fk_quadros}
+    LIMIT ${limite_linhas};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarTemperatura(id) {
+function buscarTemperatura(fk_quadros, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${id} 
-                    ORDER BY id DESC LIMIT 1`;
+	dht11_temperatura,
+    fk_quadros
+    FROM medida
+    WHERE fk_quadros = ${fk_quadros}
+    LIMIT ${limite_linhas};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
-function buscarUmidade(id) {
+function buscarUmidade(fk_quadros, limite_linhas) {
 
     var instrucaoSql = `SELECT 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        FROM medida WHERE fk_aquario = ${id} 
-                    ORDER BY id DESC LIMIT 1`;
+	dht11_umidade,
+    fk_quadros
+    FROM medida
+    WHERE fk_quadros = ${fk_quadros}
+    LIMIT ${limite_linhas};`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 
+// KPI
+
+function MaxTemperatura(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MAX(dht11_temperatura),
+    horarioMedida
+    FROM medida
+    GROUP BY horarioMedida, fk_quadros
+    WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function MaxLuminosidade(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MAX(luminosidade),
+    horarioMedida
+    FROM medida
+    GROUP BY horarioMedida, fk_quadros
+     WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function MaxUmidade(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MAX(dht11_umidade),
+    horarioMedida
+    FROM medida
+    GROUP BY horarioMedida, fk_quadros
+    WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function MinTemperatura(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MIN(dht11_temperatura),
+    horarioMedida
+FROM medida
+GROUP BY horarioMedida, fk_quadros
+    WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function MinLuminosidade(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MIN(luminosidade),
+    horarioMedida
+FROM medida
+GROUP BY horarioMedida, fk_quadros;
+    WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function MinUmidade(fk_quadros) {
+    var instrucaoSql = `
+    SELECT
+	MIN(dht11_umidade),
+    horarioMedida
+FROM medida
+GROUP BY horarioMedida, fk_quadros
+    WHERE fk_quadros = ${fk_quadros};
+    `
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
 module.exports = {
     buscarLuminosidade,
     buscarTemperatura,
-    buscarUmidade
+    buscarUmidade,
+    MaxLuminosidade,
+    MaxTemperatura,
+    MaxUmidade,
+    MinTemperatura,
+    MinLuminosidade,
+    MinUmidade
 }
